f1c3603fd46d7e0c8025ce59b57cc07fb0ba35f6
Jritmala <jenni.ritmala@hotmail.fi>
2022-01-21 16:22:20 +0200

    changes

---
+++ script.js
@@ -0,0 +1,8 @@
+alert ("Hello World!");
+alert ("Another Hello World!");
+alert ("Yet another Hello World!"); 
+
+var humanAge = 33; 
+var dogAge = humanAge * 7; 
+document.write ("Your age in dog's age is "+dogAge)
+   
