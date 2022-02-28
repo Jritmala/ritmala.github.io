fde33a8a76ed722661d569fb5c7858c0c9f75ee4
Jritmala <92936723+Jritmala@users.noreply.github.com>
2022-03-01 01:20:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+function myFunction() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("listInput").value;
+    var added = document.createTextNode(inputValue);
+    li.appendChild(added);
+    if (inputValue === '') {
+      alert("Add things to do!");
+    } else {
+      document.getElementById("toDoList").appendChild(li);
+    }
+    document.getElementById("listInput").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+}
