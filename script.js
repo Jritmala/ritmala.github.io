f2a2157384a6e7e2dc9b30240ac860c99f023953
Jritmala <92936723+Jritmala@users.noreply.github.com>
2022-03-01 01:20:03 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-alert ("Hello World!");
-alert ("Another Hello World!");
-alert ("Yet another Hello World!"); 
-
-var humanAge = 33; 
-var dogAge = humanAge * 7; 
-document.write ("Your age in dog's age is "+dogAge)
-   
